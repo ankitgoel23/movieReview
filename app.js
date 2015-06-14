68fa54254af045f6f6f726cf99c92436e2cf1481
Ankit Goel <ankit.goel@relevancelab.com>
2015-06-14 11:37:28 +0530

    adding directory for static files serve

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-var express=require('express');
+var express = require("./node_modules/express");
+var app = express();
+var path = require("path");
+var http = require("http");
+var https = require("https");
+
+    app.all('*', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        next();
+    });
+  
+	app.use('/static', express.static('public'));
+  
+  
+    app.get('/',function(req,res,next){
+		res.send('website is under construction');
+		});
+		
+	app.post('/create/movies',function(req,res,next){
+		
+		});	
+	app.get('/get/movies',function(req,res,next){
+	   
+	   });
+	   
+	   app.put('update/movies',function(req,res,next){
+		   
+		   });
+		   
+	app.delete('delete/movies',function(req,res,next){
+	
+	});
+	
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
